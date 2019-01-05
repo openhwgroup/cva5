88a18b6b54cfc3656072aeb114da567acf9c9996
Eric Matthews <ematthew@sfu.ca>
2019-01-05 12:07:13 -0800

    RAMs to read-first and smaller miss_predict logic

--- core/branch_table.sv
+++ core/branch_table.sv
@@ -39,13 +39,15 @@ module branch_table(
     } branch_table_entry_t;
 
     (* RAM_STYLE="BLOCK" *)
-    logic[$bits(branch_table_entry_t)-1:0] branch_table_tag_ram [0:BRANCH_TABLE_ENTRIES-1];
+        logic[$bits(branch_table_entry_t)-1:0] branch_table_tag_ram [0:BRANCH_TABLE_ENTRIES-1];
     branch_table_entry_t if_entry;
     branch_table_entry_t ex_entry;
+    branch_table_entry_t ex_read;
 
     (* RAM_STYLE="BLOCK" *)
-    logic [31:0] branch_table_addr_ram [0:BRANCH_TABLE_ENTRIES-1];
+        logic [31:0] branch_table_addr_ram [0:BRANCH_TABLE_ENTRIES-1];
     logic [31:0] predicted_pc;
+    logic [31:0] ex_predicted_pc;
 
     logic [31:0] miss_predict_br;
     logic [31:0] miss_predict_jal;
@@ -67,46 +69,54 @@ module branch_table(
         end
     end
 
-    //Tags and prediction
-    always_ff @(posedge clk) begin
-        if (bt.branch_ex) begin
-            branch_table_tag_ram[bt.ex_pc[BADDR_W+1:2]] <= ex_entry;
-        end
-    end
-    always_ff @(posedge clk) begin
-        if (bt.new_mem_request)
-            if_entry <=  branch_table_tag_ram[bt.next_pc[BADDR_W+1:2]];
-    end
-    //branch address
-    always_ff @(posedge clk) begin
-        if (bt.branch_ex) begin
-            branch_table_addr_ram[bt.ex_pc[BADDR_W+1:2]] <= ( bt.branch_taken ? bt.jump_pc : bt.njump_pc);
-        end
-    end
-    always_ff @(posedge clk) begin
-        if (bt.new_mem_request)
-            predicted_pc <=  branch_table_addr_ram[bt.next_pc[BADDR_W+1:2]];
-    end
+
+    byte_en_BRAM #(.LINES(BRANCH_TABLE_ENTRIES)) branch_tag_ram (
+            .clk(clk),
+            .addr_a(bt.ex_pc[BADDR_W+1:2]),
+            .en_a(bt.branch_ex),
+            .be_a('1),
+            .data_in_a(ex_entry),
+            .data_out_a(),
+
+            .addr_b(bt.next_pc[BADDR_W+1:2]),
+            .en_b(bt.new_mem_request),
+            .be_b('0),
+            .data_in_b('0),
+            .data_out_b(if_entry));
+
+    byte_en_BRAM #(.LINES(BRANCH_TABLE_ENTRIES)) branch_address_ram (
+            .clk(clk),
+            .addr_a(bt.ex_pc[BADDR_W+1:2]),
+            .en_a(bt.branch_ex),
+            .be_a('1),
+            .data_in_a(bt.branch_taken ? bt.jump_pc : bt.njump_pc),
+            .data_out_a(),
+
+            .addr_b(bt.next_pc[BADDR_W+1:2]),
+            .en_b(bt.new_mem_request),
+            .be_b('0),
+            .data_in_b('0),
+            .data_out_b(predicted_pc));
+
     //Predict next branch to same location/direction as current
     assign ex_entry.valid = 1;
     assign ex_entry.tag = bt.ex_pc[31:32-BTAG_W];
     assign ex_entry.use_ras = bt.is_return_ex;
 
-    assign miss_predict = bt.branch_ex && (
-        (bt.branch_taken && bt.dec_pc != bt.jump_pc) ||
-        (~bt.branch_taken && bt.dec_pc != bt.njump_pc));
-
+    assign miss_predict = bt.branch_ex && ~(
+            {bt.branch_taken, bt.dec_pc} == {1'b1, bt.jump_pc} ||
+            {bt.branch_taken, bt.dec_pc} == {1'b0, bt.njump_pc});
 
     assign tag_match = ({if_entry.valid, if_entry.tag} == {1'b1, bt.if_pc[31:32-BTAG_W]});
     assign bt.predicted_pc = predicted_pc;
 
     generate if (USE_BRANCH_PREDICTOR) begin
-        assign bt.use_prediction = tag_match;
-        assign bt.flush = miss_predict;
-    end else begin
-        assign bt.use_prediction = 0;
-        assign bt.flush = bt.branch_ex & bt.branch_taken;
-    end endgenerate
+            assign bt.use_prediction = tag_match;
+            assign bt.flush = miss_predict;
+        end else begin
+            assign bt.use_prediction = 0;
+            assign bt.flush = bt.branch_ex & bt.branch_taken;
+        end endgenerate
 
     assign bt.use_ras = if_entry.use_ras;
 
