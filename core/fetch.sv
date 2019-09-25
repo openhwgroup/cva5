580dd141395c0f134e2a93abd6d6acbaedad4a2d
Eric Matthews <ematthew@sfu.ca>
2019-09-25 12:29:20 -0700

    fetch cleanups

--- core/fetch.sv
+++ core/fetch.sv
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017, 2018, 2019 Eric Matthews,  Lesley Shannon
+ * Copyright © 2017-2019 Eric Matthews,  Lesley Shannon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,13 +53,16 @@ module fetch(
         );
 
     localparam NUM_SUB_UNITS = USE_I_SCRATCH_MEM + USE_ICACHE;
-    localparam NUM_SUB_UNITS_W = $clog2(NUM_SUB_UNITS);
+    localparam NUM_SUB_UNITS_W = (NUM_SUB_UNITS == 1) ? 1 : $clog2(NUM_SUB_UNITS);
 
     localparam BRAM_ID = 0;
     localparam ICACHE_ID = USE_I_SCRATCH_MEM;
 
-    fetch_sub_unit_interface fetch_sub[NUM_SUB_UNITS-1:0]();
+    localparam FETCH_BUFFER_DEPTH_W = $clog2(FETCH_BUFFER_DEPTH);
+    localparam NEXT_ID_DEPTH = USE_ICACHE ? 2 : 1;
 
+    //Subunit signals
+    fetch_sub_unit_interface fetch_sub[NUM_SUB_UNITS-1:0]();
     logic [NUM_SUB_UNITS-1:0] sub_unit_address_match;
     logic [NUM_SUB_UNITS-1:0] last_sub_unit_id;
     logic [NUM_SUB_UNITS-1:0] unit_ready;
@@ -68,33 +71,26 @@ module fetch(
     logic [31:0] anded_unit_data_array [NUM_SUB_UNITS-1:0];
 
     logic units_ready;
-
+    logic units_data_valid;
 
     logic [31:0] next_pc;
     logic [31:0] if_pc;
+    logic update_pc;
 
-    localparam LOG2_FETCH_BUFFER_DEPTH = $clog2(FETCH_BUFFER_DEPTH);
-
-    logic [LOG2_FETCH_BUFFER_DEPTH:0] inflight_count;
+    logic flush_or_rst;
+    logic [FETCH_BUFFER_DEPTH_W:0] inflight_count;
+    fifo_interface #(.DATA_WIDTH(NUM_SUB_UNITS_W)) next_unit();
     logic space_in_inst_buffer;
     logic mem_ready;
     logic new_mem_request;
 
-    logic new_issue;
-    logic mem_valid;
-
+    //Cache related
     logic delayed_flush;
-
     logic [31:0] stage2_phys_address;
     logic stage2_valid;
 
-    logic update_pc;
-
-    logic [31:0] final_instruction;
-
-
-    /////////////////////////////////////////
-
+    ////////////////////////////////////////////////////
+    //Implementation
     genvar i;
     generate
         for(i=0; i < NUM_SUB_UNITS; i++) begin
@@ -103,9 +99,11 @@ module fetch(
         end
     endgenerate
     assign units_ready = &unit_ready;
+    assign units_data_valid = |unit_data_valid;
 
-    assign update_pc = mem_ready | gc_fetch_flush;
 
+
+    ////////////////////////////////////////////////////
     //Fetch PC
     always_ff @(posedge clk) begin
         if (rst)
@@ -129,9 +127,9 @@ module fetch(
     assign bp.next_pc = next_pc;
 
     assign bp.if_pc = if_pc;
-    /*************************************
-     * TLB
-     *************************************/
+
+    ////////////////////////////////////////////////////
+    //TLB
     assign tlb.virtual_address = if_pc;
     assign tlb.execute = 1;
     assign tlb.rnw = 0;
@@ -141,25 +139,37 @@ module fetch(
             stage2_phys_address <= tlb.physical_address;
         end
     end
+
     //////////////////////////////////////////////
+    //Issue Control Signals
+    assign flush_or_rst = (rst | gc_fetch_flush);
 
     always_ff @(posedge clk) begin
-        if (rst | gc_fetch_flush)
+        if (flush_or_rst)
             inflight_count <= '1;
         else
-            inflight_count <= inflight_count - (LOG2_FETCH_BUFFER_DEPTH+1)'(mem_ready) + (LOG2_FETCH_BUFFER_DEPTH+1)'(pre_decode_pop);
+            inflight_count <= inflight_count - (FETCH_BUFFER_DEPTH_W+1)'(mem_ready) + (FETCH_BUFFER_DEPTH_W+1)'(pre_decode_pop);
     end
-    assign space_in_inst_buffer = inflight_count[LOG2_FETCH_BUFFER_DEPTH];
+    assign space_in_inst_buffer = inflight_count[FETCH_BUFFER_DEPTH_W];
 
-//assign space_in_inst_buffer = inflight_count < FETCH_BUFFER_DEPTH;
     assign mem_ready = tlb.complete & space_in_inst_buffer & units_ready;
     assign new_mem_request = mem_ready & (~gc_fetch_flush);
+    assign update_pc = mem_ready | gc_fetch_flush;
 
+    //////////////////////////////////////////////
+    //Subunit Tracking
+    assign next_unit.push = mem_ready;
+    assign next_unit.pop = units_data_valid;
+    one_hot_to_integer #(NUM_SUB_UNITS) hit_way_conv (.*, .one_hot(sub_unit_address_match), .int_out(next_unit.data_in));
+    taiga_fifo #(.DATA_WIDTH(NUM_SUB_UNITS_W), .FIFO_DEPTH(NEXT_ID_DEPTH), .FIFO_TYPE(LUTRAM_FIFO))
+        attributes_fifo (.fifo(next_unit), .rst(flush_or_rst), .*);
+
+    ////////////////////////////////////////////////////
     //Memory interfaces
     generate if (USE_I_SCRATCH_MEM) begin
             ibram i_bram (.*, .fetch_sub(fetch_sub[BRAM_ID]));
             assign sub_unit_address_match[BRAM_ID] = (tlb.physical_address[31:32-SCRATCH_BIT_CHECK] == SCRATCH_ADDR_L[31:32-SCRATCH_BIT_CHECK]);
-            assign fetch_sub[BRAM_ID].new_request = new_mem_request & (USE_ICACHE ? ~sub_unit_address_match[ICACHE_ID] : 1'b1);
+            assign fetch_sub[BRAM_ID].new_request = new_mem_request & (USE_ICACHE ? ~sub_unit_address_match[ICACHE_ID] : 1'b1);//Pass invalid address fetches through scratchmem
             assign fetch_sub[BRAM_ID].stage1_addr = tlb.physical_address;
             assign fetch_sub[BRAM_ID].stage2_addr = stage2_phys_address;
             assign unit_data_array[BRAM_ID] = fetch_sub[BRAM_ID].data_out;
@@ -174,11 +184,11 @@ module fetch(
             assign unit_data_array[ICACHE_ID] = fetch_sub[ICACHE_ID].data_out;
 
             always_ff @(posedge clk) begin
-                if(rst | gc_fetch_flush)
+                if(flush_or_rst)
                     stage2_valid <= 0;
                 else if (mem_ready)
                     stage2_valid <= 1;
-                else if (new_issue)
+                else if (pre_decode_push)
                     stage2_valid <= 0;
             end
 
@@ -202,23 +212,11 @@ module fetch(
         end
     endgenerate
 
-    assign mem_valid = ~(gc_fetch_flush | delayed_flush);
-    assign new_issue =  mem_valid & (|unit_data_valid);
-
-    //bitwise AND all subunit outputs with valid signal then or all outputs together
-    always_comb begin
-        final_instruction = 0;
-        foreach (unit_data_array[i]) begin
-            anded_unit_data_array[i] = unit_data_array[i] & {$bits(unit_data_array[i]){unit_data_valid[i]}};
-            final_instruction |= anded_unit_data_array[i];
-        end
-    end
-
     ////////////////////////////////////////////////////
     //Pre-Decode Output
-    assign pre_decode_instruction = final_instruction;
+    assign pre_decode_instruction = unit_data_array[next_unit.data_out];
     assign pre_decode_pc = stage2_phys_address;
-    assign pre_decode_push = new_issue;
+    assign pre_decode_push = ~(gc_fetch_flush | delayed_flush) & units_data_valid;
 
     always_ff @(posedge clk) begin
         if (new_mem_request) begin
